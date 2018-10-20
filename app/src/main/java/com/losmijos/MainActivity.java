71892e91f37579c682a4a8bb35fdc38bb0478d51
MixxaVega <wendyv.vega@gmail.com>
2018-10-20 14:58:17 -0600

    Se creo ventana de agregar suministro
    Se agrego la actividad de agregar un suministro y se modifico la base de
    datos

--- app/src/main/java/com/losmijos/MainActivity.java
+++ app/src/main/java/com/losmijos/MainActivity.java
@@ -1,13 +1,30 @@
 package com.losmijos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button botoncito;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botoncito = findViewById(R.id.btn_siguiente);
+        botoncito.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_siguiente:
+                Intent i = new Intent(MainActivity.this, agregarSuministro.class);
+                startActivity(i);
+                break;
+        }
     }
 }
