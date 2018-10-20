6a75e6dd02bd3bf5b19062cf2c57f5f0885e826e
ODanielHernandez <danihm95@gmail.com>
2018-10-20 11:17:27 -0600

    [ADD] Listview Adapter ready

--- app/src/main/java/com/losmijos/MainActivity.java
+++ app/src/main/java/com/losmijos/MainActivity.java
@@ -2,12 +2,42 @@ package com.losmijos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView lista;
+    private adaptador adaptador;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lista = (ListView) findViewById(R.id.listaDestrastres);
+        adaptador = new adaptador(this, getArrayListItems());
+        lista.setAdapter(adaptador);
+
     }
+
+    private ArrayList<entidad> getArrayListItems(){
+        ArrayList<entidad> listItems = new ArrayList<>();
+        //inundaciones, terremotos, huracanes, incendios forestales, tornados, tsunami, temperaturas extremas, deslizamiento de tierra, sequias, erupción volcanica
+        listItems.add(new entidad(R.drawable.flood,"Inundaciones","Aguas no te vayas a mojar"));
+        listItems.add(new entidad(R.drawable.earthquake,"Terremotos","Choque de placas tectonicas"));
+        listItems.add(new entidad(R.drawable.wind,"Huracan","Viene el viento y tira las casas"));
+        listItems.add(new entidad(R.drawable.forest_fire,"Incendio Forestal","Fuego en nuestros bosques"));
+        listItems.add(new entidad(R.drawable.tornado,"Tornado","Vientorrazo que se lleva todo"));
+        listItems.add(new entidad(R.drawable.tsunami,"Tsunami","Olas gigantes chocan"));
+        listItems.add(new entidad(R.drawable.temperature,"Temperaturas extremas","Muy caliente"));
+        listItems.add(new entidad(R.drawable.landslide,"Deslizamiento de tierra","Montaña, colina o cerro se está cayendo"));
+        listItems.add(new entidad(R.drawable.drought,"Sequía","Se está secando la tierra"));
+        listItems.add(new entidad(R.drawable.volcano,"Erupciones volcanicas","Un volcan está apunto de eyacular"));
+
+        return listItems;
+    }
+
 }
---
+++ app/src/main/java/com/losmijos/adaptador.java
@@ -0,0 +1,56 @@
+package com.losmijos;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class adaptador extends BaseAdapter{
+
+    private Context context;
+    private ArrayList<entidad> listItem;
+
+    public adaptador(Context context, ArrayList<entidad> listItem) {
+        this.context = context;
+        this.listItem = listItem;
+    }
+
+    @Override
+    public int getCount() {
+        return listItem.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+
+        return listItem.get(position);
+
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        entidad Item = (entidad) getItem(position);
+
+
+        convertView = LayoutInflater.from(context).inflate(R.layout.list, null);
+        ImageView imgFoto = (ImageView) convertView.findViewById(R.id.imgFoto);
+        TextView titulo = (TextView) convertView.findViewById(R.id.titulo);
+        TextView contenido = (TextView) convertView.findViewById(R.id.contenido);
+
+        imgFoto.setImageResource(Item.getImgFoto());
+        titulo.setText(Item.getTitulo());
+        contenido.setText(Item.getContenido());
+        return convertView;
+    }
+}
+
