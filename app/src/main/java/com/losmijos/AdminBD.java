8fa79785cf34f0999ac3e56a1943d138dd02f0b8
MixxaVega <wendyv.vega@gmail.com>
2018-10-20 23:42:03 -0600

    Cambios en la base de datos y agregar suministros

--- app/src/main/java/com/losmijos/AdminBD.java
+++ app/src/main/java/com/losmijos/AdminBD.java
@@ -17,21 +17,52 @@ public class AdminBD extends SQLiteOpenHelper {
 
     public String crearTablas(String nombreTabla){
         String CREARTABLA= "CREATE TABLE "+nombreTabla+" (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                "articulo TEXT, importancia INTEGER, verificado INTEGER);";
+                "articulo TEXT, importancia INTEGER, verificado BOOLEAN);";
         return CREARTABLA;
     }
     @Override
     public void onCreate(SQLiteDatabase bd) {
-        bd.execSQL(crearTablas("incendios"));
+        bd.execSQL(crearTablas("inundaciones"));
+        insertarRegistro(bd,"Agua potable", 5, "inundaciones");
+        insertarRegistro(bd,"Alimentos no perecederos", 5, "inundaciones");
+        insertarRegistro(bd,"Iluminación portátil", 5, "inundaciones");
+
         bd.execSQL(crearTablas("terremotos"));
+        insertarRegistro(bd,"Cubre bocas", 5, "terremotos");
+        insertarRegistro(bd,"Agua potable", 5, "terremotos");
+        insertarRegistro(bd,"Herramienta múltiple", 5, "terremotos");
+
         bd.execSQL(crearTablas("huracanes"));
-        bd.execSQL(crearTablas("incendiosForestales"));
+        insertarRegistro(bd,"Comida enlatada", 5, "huracanes");
+        insertarRegistro(bd,"Articulos de primeros auxilios", 5, "huracanes");
+        insertarRegistro(bd,"Agua potable", 5, "huracanes");
+
+        //bd.execSQL(crearTablas("incendiosForestales"));
         bd.execSQL(crearTablas("tornados"));
+        insertarRegistro(bd,"Comida enlatada", 5, "tornados");
+        insertarRegistro(bd,"Articulos de primeros auxilios", 5, "tornados");
+        insertarRegistro(bd,"Agua potable", 5, "tornados");
+
+
         bd.execSQL(crearTablas("tsunamis"));
+        insertarRegistro(bd,"Mapas locales", 5, "tsunamis");
+        insertarRegistro(bd,"Botiquín de primeros auxilios", 5, "tsunamis");
+        insertarRegistro(bd,"Agua potable", 5, "tsunamis");
+
         bd.execSQL(crearTablas("temperaturasExtremas"));
-        bd.execSQL(crearTablas("deslizamientoDeTierra"));
+        insertarRegistro(bd,"Agua potable", 5, "temperaturasExtremas");
+        insertarRegistro(bd,"Compresas frías", 5, "temperaturasExtremas");
+
+        //bd.execSQL(crearTablas("deslizamientoDeTierra"));
+
         bd.execSQL(crearTablas("sequias"));
+        insertarRegistro(bd,"Agua potable", 5, "sequias");
+        insertarRegistro(bd,"Alimento no perecedero", 5, "sequias");
+
         bd.execSQL(crearTablas("erupcionVolcanica"));
+        insertarRegistro(bd,"Máscara para polvo", 5, "erupcionVolcanica");
+        insertarRegistro(bd,"Ropa de repuesto", 5, "erupcionVolcanica");
+        insertarRegistro(bd,"Gafas desechables", 5, "erupcionVolcanica");
     }
 
     @Override
@@ -40,7 +71,7 @@ public class AdminBD extends SQLiteOpenHelper {
     }
 
     public void insertarRegistro(SQLiteDatabase bd, String BDarticulo, int BDimportancia, String nombreTabla){
-        final String insertar = "INSERT INTO "+nombreTabla+"(articulo, importancia, verificado) VALUES('"+BDarticulo+"','"+BDimportancia+"',0);";
+        final String insertar = "INSERT INTO "+nombreTabla+"(articulo, importancia, verificado) VALUES('"+BDarticulo+"','"+BDimportancia+"',False);";
         bd.execSQL(insertar);
     }
 
--- app/src/main/java/com/losmijos/agregarSuministro.java
+++ app/src/main/java/com/losmijos/agregarSuministro.java
@@ -1,22 +1,35 @@
 package com.losmijos;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class agregarSuministro extends AppCompatActivity {
+public class agregarSuministro extends AppCompatActivity implements View.OnClickListener{
     Spinner spinner;
     String[] numero = {"1","2","3","4","5"};
+    ImageView btnAceptar;
+    ImageView btnCancelar;
+    EditText articulo;
+    int importancia;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_suministro);
 
         spinner = (Spinner) findViewById(R.id.spinner);
+        btnAceptar = findViewById(R.id.btnAceptar);
+        articulo = findViewById(R.id.txtSuministro);
+        btnCancelar = findViewById(R.id.btnCancelar);
+        btnAceptar.setOnClickListener(this);
+        btnCancelar.setOnClickListener(this);
 
         spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, numero));
 
@@ -27,11 +40,29 @@ public class agregarSuministro extends AppCompatActivity {
             {
                 Toast.makeText(adapterView.getContext(),
                         (String) adapterView.getItemAtPosition(pos), Toast.LENGTH_SHORT).show();
+                importancia = (Integer) adapterView.getItemAtPosition(pos);
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent)
             {    }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btnAceptar:
+                Intent i = getIntent();
+                i.putExtra("articulo", articulo.getText().toString());
+                i.putExtra("importancia", importancia);
+                setResult(RESULT_OK, i);
+                finish();
+                break;
+            case R.id.btnCancelar:
+                setResult(RESULT_CANCELED);
+                finish();
+                break;
+        }
     }
 }
